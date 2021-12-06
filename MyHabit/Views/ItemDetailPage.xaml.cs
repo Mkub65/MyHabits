5bd1d428703b8a916458d091d4ba1050ee018213
Jan Wagrowski <jan.wagrowski@pl.abb.com>
2021-12-06 20:09:16 +0100

    Add project files.

---
+++ MyHabit/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MyHabit.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MyHabit.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ MyHabit/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MyHabit.Models;
+using MyHabit.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyHabit.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
